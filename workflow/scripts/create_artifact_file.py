08c10c4af235374c507302939f9696a96f71b3da
jonca79 <jonas.almlof@igp.uu.se>
2022-03-30 11:41:45 +0200

    fix: bugfix

--- workflow/scripts/create_artifact_file.py
+++ workflow/scripts/create_artifact_file.py
@@ -83,4 +83,4 @@ for key in FFPE_call_dict:
                 the result should be an unbiased estimate of the true population variance.'''
                 sd_af = statistics.stdev(FFPE_call_dict[key][caller][1])
             artifact_panel.write("\t" + str(FFPE_call_dict[key][caller][0]) + "\t" + str(median_af) + "\t" + str(sd_af))
-        artifact_panel.write("\n")
+    artifact_panel.write("\n")
