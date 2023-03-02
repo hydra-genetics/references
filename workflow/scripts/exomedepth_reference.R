e1e16274dc6343dd1b5df991c3325a7559d6a615
Padraic Corcoran <padraic.corcoran@scilifelab.uu.se>
2023-03-02 16:31:34 +0100

    Change to save df in script

--- workflow/scripts/exomedepth_reference.R
+++ workflow/scripts/exomedepth_reference.R
@@ -2,14 +2,11 @@ library(ExomeDepth)
 
 bam_df <- read.delim(snakemake@input[["bam_list_file"]], header = FALSE)
 bam_list <- bam_df$V1
-exons_bed <- read.csv(snakemake@params[["bed"]], header = FALSE, sep = "\t")
+exons_bed <- read.delim(snakemake@params[["bed"]], header = FALSE, sep = "\t")
 
 # Create counts dataframe for all BAMs
-my_counts <- getBamCounts(bed.frame = exons_bed,
+refcount_df <- getBamCounts(bed.frame = exons_bed,
                           bam.files = bam_list,
                           include.chr = FALSE)
 
-# prepare the main matrix of read count data
-refcount_mat <- as.matrix(
-    my_counts[, grep(names(my_counts), pattern = ".bam$")])
-save(refcount_mat, file = snakemake@output[["reference"]])
+save(refcount_df, file = snakemake@output[["reference"]])
